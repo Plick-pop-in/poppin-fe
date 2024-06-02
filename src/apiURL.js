6e434bd6ade9e14f58a3d546eebc2abf06287e40
standyoon <81007682+Byunseoyoon@users.noreply.github.com>
2024-06-02 22:09:14 +0900

    refactor: https -> http 변경

--- src/apiURL.js
+++ src/apiURL.js
@@ -1,12 +1,12 @@
 const apiURLs = {
-    popuplist: "https://www.plick.shop/v1/popup/popuplist",
-    popupdetail: "https://www.plick.shop/v1/popup/popupdetail/",
-    randomImages: "https://www.plick.shop/v1/popup/random-images",
-    top3: "https://www.plick.shop/v1/popup/top3",
-    inProgress: "https://www.plick.shop/v1/popup/in-progress",
-    chat: "https://www.plick.shop/v1/chat",
-    chatList: "https://www.plick.shop/v1/chat/userchatlist",
-    live: "https://www.plick.shop/v1/live",
+    popuplist: "http://www.plick.shop/v1/popup/popuplist",
+    popupdetail: "http://www.plick.shop/v1/popup/popupdetail/",
+    randomImages: "http://www.plick.shop/v1/popup/random-images",
+    top3: "http://www.plick.shop/v1/popup/top3",
+    inProgress: "http://www.plick.shop/v1/popup/in-progress",
+    chat: "http://www.plick.shop/v1/chat",
+    chatList: "http://www.plick.shop/v1/chat/userchatlist",
+    live: "http://www.plick.shop/v1/live",
     // 다른 링크들도 필요에 따라 추가 가능
   };
   
