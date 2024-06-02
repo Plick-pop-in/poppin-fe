c9c0523369329fc9248f602c860f38cf929a2243
Yerin Song <yrsong0915@gmail.com>
2024-06-02 20:21:55 +0900

    feat: https 로 변경

--- src/apiURL.js
+++ src/apiURL.js
@@ -1,10 +1,10 @@
 const apiURLs = {
-    randomImages: "http://www.plick.shop/v1/popup/random-images",
-    top3: "http://www.plick.shop/v1/popup/top3",
-    inProgress: "http://www.plick.shop/v1/popup/in-progress",
-    chat: "http://www.plick.shop/v1/chat",
-    chatList: "http://www.plick.shop/v1/chat/userchatlist",
-    live: "http://www.plick.shop/v1/live",
+    randomImages: "https://www.plick.shop/v1/popup/random-images",
+    top3: "https://www.plick.shop/v1/popup/top3",
+    inProgress: "https://www.plick.shop/v1/popup/in-progress",
+    chat: "https://www.plick.shop/v1/chat",
+    chatList: "https://www.plick.shop/v1/chat/userchatlist",
+    live: "https://www.plick.shop/v1/live",
     // 다른 링크들도 필요에 따라 추가 가능
   };
   
