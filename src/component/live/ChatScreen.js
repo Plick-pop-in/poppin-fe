856605095127ef1cb9fbb00effad365b41a16f78
Yerin Song <yrsong0915@gmail.com>
2024-06-03 00:10:12 +0900

    ui: 채팅창 최종 ui

--- src/component/live/ChatScreen.js
+++ src/component/live/ChatScreen.js
@@ -5,12 +5,12 @@ const ChatScreen = () => {
     return (
         <div className="chat-screen">
             <div className="chat-messages">
-                {/* 채팅 메시지가 표시될 곳입니다. */}
+                {/* 채팅 메시지가 표시될 곳*/}
             </div>
             <div className="chat-input">
-                <input className="chat-input-box" type="text" placeholder="메시지를 입력하세요..." />
+                <input className="chat-input-box" type="text" placeholder="메시지를 입력하세요." />
                 <button className="chat-input-button" aria-label="전송">
-                    <img src="//src/assets/images/ic_chat_send.png" alt="" />
+                <img src="/src/assets/images/ic_chat_send.png" alt="" />
                 </button>
             </div>
         </div>
