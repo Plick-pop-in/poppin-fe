c0e081ec0a08f0803f32cb674a1c4fe867d2e257
standyoon <81007682+Byunseoyoon@users.noreply.github.com>
2024-06-03 03:42:18 +0900

    feat: heart 진행중

--- src/component/heart/Heart.js
+++ src/component/heart/Heart.js
@@ -1,16 +1,34 @@
-import React from "react";
+import React, { useState } from "react";
+import axios from "axios";
+import useCustomLogin from '../myPage/module/useCustomLogin';
 
 const Heart = (item) =>{
+    //스타일
     const imgStyle = {
         marginRight: '5px',
         verticalAlign: 'middle',
-        width: '18px'
+        width: '18px',
+        cursor: "pointer"
+    };
+    
+    //
+    const [isLiked, setIsLiked] = useState(false);
+    const { isLogin, moveToLogin, moveToPath } = useCustomLogin()
+
+    const handleClick = () => {
+        if(!isLogin){
+            moveToLogin();
+            return;
+        }
+
+       
     };
 
+    
     // 유저 로그인시 아닐시 차이 있게
     return(
         <div className="heart">
-            <img src={require("../../assets/images/heart.png")} style={imgStyle} />
+            <img src={isLiked ? require("../../assets/images/heartColor.png") : require("../../assets/images/heart.png")} style={imgStyle} onClick={handleClick}/>
             {item.heart > 999 ? (<span>999+</span>) : (<span>{item.heart}</span>)}
         </div>
     );
