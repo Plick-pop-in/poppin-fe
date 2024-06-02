217f1eba6a278ed9b87c7378f66a10034002acb4
hyo-limilimee <1001lily@naver.com>
2024-06-03 00:10:13 +0900

    feat: 박스에 내용 매핑

--- src/component/live/LiveBox.js
+++ src/component/live/LiveBox.js
@@ -11,17 +11,17 @@ const LiveBox = (props) => {
 
     return (
         <div className="list-box">
-            <img className="chat-box-img" src={require('../../assets/images/' + props.image + '.png')} />
+            <img className="chat-box-img" src={props.image} />
             <div className="chat-box-content">
                 <div className="chat-box-basic-info">
                     <div className="chat-box-name"><strong>{props.name}</strong></div>
-                    <div className="chat-box-date"> {props.start_date}-{props.end_date}</div>
+                    <div className="chat-box-date">{props.start_date}-{props.end_date}</div>
                     <div className="chat-box-address">{props.city} {props.local} {props.location}</div>
                 </div>
                 <div className="chat-box-join-info">
                     <div className="chat-box-liked-content">
                     <img className="chat-box-ic-liked" src={require('../../assets/images/ic_person.png')} />
-                        <div className="chat-box-liked">100명이 와글와글</div>
+                        <div className="chat-box-liked">{props.joinedPeople}명이 와글와글</div>
                     </div>
                     <div className="chat-box-point">Ⓒ100P</div>
                     <button className="join-button"><strong>join</strong></button>
