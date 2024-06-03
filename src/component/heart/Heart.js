42817f85ce3958aa78d558b21ee58fcdb3fb856a
standyoon <81007682+Byunseoyoon@users.noreply.github.com>
2024-06-03 18:12:20 +0900

    feat: 하트 api 붙이기 수정중

--- src/component/heart/Heart.js
+++ src/component/heart/Heart.js
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from "react";
 import axios from "axios";
 import useCustomLogin from '../myPage/module/useCustomLogin';
 import {useSelector} from "react-redux";
+import apiURLs from "../../apiURL";
 
 
 const Heart = (item) =>{
@@ -17,26 +18,23 @@ const Heart = (item) =>{
     const { isLogin, moveToLogin, moveToPath } = useCustomLogin();
     const loginInfo = useSelector(state => state.loginSlice);
 
+    //초기 렌더링 시 좋아요 상태를 서버에서 가져와 설정
     useEffect(() => {
         const userId = loginInfo.id;
         console.log('User ID:', userId);
-    }, [loginInfo]);
 
-    useEffect(() => {
-        // 초기 렌더링 시 좋아요 상태를 서버에서 가져와 설정
-        axios.get("/api/like/status", { popupId: item.popupId })
-            .then(response => {
-                setIsLiked(response.data.isLiked);
-            })
-            .catch(error => {
-                console.error("Error:", error);
-            });
-    }, [item.popupId]);
+        if(loginInfo){
+            const url = `http://localhost:8080/v1/heart/isLike?popupId=?${item.popupId}&userId=${loginInfo.id}`;
+            axios.get(url)
+                .then(response => {
+                    setIsLiked(response.data.isLiked);
+                })
+                .catch(error => {
+                    console.error("Error:", error);
+                });
+        }
+    }, [loginInfo, item.popupId]);
 
-
-    useEffect(()=>{
-
-    }, []);
 
     const handleClick = () => {
         if(!isLogin){
@@ -44,7 +42,28 @@ const Heart = (item) =>{
             return;
         }
 
-       
+        // 좋아요 해제 요청 보내기
+        if(isLiked){
+             axios.delete("http://localhost:8080/v1/heart/deleteHeart", { popupId: item.popupId, userId: loginInfo.id } )
+             .then(response => {
+                item.heart = response.data.likeCount
+                 //setHeartCount(response.data.heart);    //heart 수 다시 가져오는거 해야함 ㅋ..
+                 setIsLiked(false);
+             })
+             .catch(error => {
+                 console.error("Error:", error);
+             });
+        }else{
+            // 좋아요 추가 요청 보내기
+            axios.post("http://localhost:8080/v1/heart/addHeart",{ popupId: item.popupId, userId: loginInfo.id }  )
+                .then(response => {
+                    //setHeartCount(response.data.heart); //heart 수 다시 가져오는거 해야함 ㅋ..
+                    setIsLiked(true);
+                })
+                .catch(error => {
+                    console.error("Error:", error);
+                });
+        }
     };
 
     
