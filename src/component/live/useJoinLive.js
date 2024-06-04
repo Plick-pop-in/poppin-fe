798be57345f99c0cfdd4153f0aa0c6416633fee4
standyoon <81007682+Byunseoyoon@users.noreply.github.com>
2024-06-05 04:28:09 +0900

    feat: 잘 가져오는 구만

--- src/component/live/useJoinLive.js
+++ src/component/live/useJoinLive.js
@@ -2,31 +2,34 @@ import React, { useEffect, useState } from "react";
 import useCustomLogin from "../myPage/module/useCustomLogin";
 import {useSelector} from "react-redux";
 import axios from "axios";
+import apiURLs from "../../apiURL";
 
-const useJoinLive=()=>{
+const useJoinLive=(popupId)=>{
     const { isLogin, moveToLogin } = useCustomLogin();
     const loginInfo = useSelector(state => state.loginSlice);
 
+    const postAPI= async() =>{
+        //알람창 띄우기
+        
+            //await axios.post("http://localhost:8080/v1/chat/joinLive", new URLSearchParams({ userId: loginInfo.id, popupId: parseInt(popupId), minusPoint:100 }))
+            await axios.post(apiURLs.joinLive, new URLSearchParams({ userId: loginInfo.id, popupId: parseInt(popupId), minusPoint:100 }))
+            .then((response) => {
+                console.log("response", response);
+                alert('포인트 차감이 완료되었습니다.');
+            })
+            .catch(error => {
+                console.error("Error:", error);
+                alert('결제 에러 발생');
+            });
+    }
 
-    const JoinLive = async(popupId) =>{
+    const JoinLive = () =>{
         //로그인 안했으면 로그인 창으로 이동
         if(!isLogin){
             moveToLogin();
-            
-        }else{
-            //알람창 띄우기
-            if(window.confirm("[알림] Live 채팅방에 입장하시려면 100포인트가 차감됩니다. \n 입장 하시겠습니까?")){
-                const url = "";
-                await axios
-                .post(url, new URLSearchParams({ popupId, userId: loginInfo.id, minusPoint:100 }) )
-                .then(response => {
-                    console.log("response", response);
-                    alert('포인트 차감이 완료되었습니다.');
-                })
-                .catch(error => {
-                    console.error("Error:", error);
-                    alert('결제 에러 발생');
-                });
+        }else if(loginInfo.id){
+            if(window.confirm("[ Live 채팅 안내 ]\nLive 채팅방에 참여하시려면 100포인트가 차감됩니다. \n참여 하시겠습니까?")){
+                postAPI();
             }else{
 
             }
