3c56e4f0280d133e040319f8136b6640b03e80d9
Yerin Song <yrsong0915@gmail.com>
2024-06-07 12:25:19 +0900

    ui: 문구 추가

--- src/component/myPage/Mychat.js
+++ src/component/myPage/Mychat.js
@@ -9,6 +9,11 @@ class Mychat extends React.Component {
       <div className="mychat">
         <SidebarComponent />
         <div className="main-content">
+        <div className="my-chat-title">
+                <strong style={{ fontSize: '36px' }}>구매한 채팅방 목록</strong>
+                <br /><br />
+                <strong style={{ fontSize: '24px' }}>클릭 시 채팅 화면으로 이동합니다.</strong>
+            </div>
             < ChatBox/>
         </div>
       </div>
