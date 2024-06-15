67d3ac7adf989bc7b307c0cd1f324166cc4429bd
Yerin Song <yrsong0915@gmail.com>
2024-06-15 22:21:28 +0900

    feat: 채팅방에서 명수 삭제

--- src/component/live/Chat.js
+++ src/component/live/Chat.js
@@ -9,24 +9,19 @@ const Chat = () => {
     const { popupName } = useParams(); // React Router의 useParams를 사용하여 URL 파라미터를 받아옴
     const queryParams = new URLSearchParams(location.search);
     const chatboxname = popupName || queryParams.get("chatboxname") || "채팅 이름 찾을 수 없음";
-    const joinedPeople = queryParams.get("joinedPeople") || 100; // 기본값을 100으로 설정
 
     const handleChatBoxClick = () => {
         window.location.reload(); // 페이지를 새로고침
     };
 
     console.log("chatboxname:", chatboxname);
-    console.log("joinedPeople:", joinedPeople);
+
 
     return (
         <div className="chat-page-background">
             <div className="chat-page-top">
                 <div className="chat-title">
                     {chatboxname}
-                </div>
-                <div className="chat-join-info">
-                    <img className="chat-ic-person" src={require('../../assets/images/ic_person.png')} alt="person icon" />
-                    <div className="chat-person">{joinedPeople}명이 와글와글</div>
                 </div>
             </div>
             <div className="chat-white-background">
